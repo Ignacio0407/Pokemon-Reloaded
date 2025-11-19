92ca77846d8a9d5b1ccc517b90c80babb5833dd6
Ignacio0407 <igngutser@alum.us.es>
2025-11-19 10:11:22 +0100

    Updated gitignore to ignore the exe file

--- start.bat
+++ start.bat
@@ -3,4 +3,4 @@ REM Ejecuta el script de Python en segundo plano
 start "" python auto_commit.py
 
 REM Lanza el juego
-start "" "Proyecto Reloaded The Last Beta 1.8.5.exe"
+start "" "Proyecto-Reloaded-The-Last-Beta-1.8.5.exe"
